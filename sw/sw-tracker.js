b6c5163475f8767ba6dca9576db42e7f394c8b25
Edward B <57622377+P4tt4te@users.noreply.github.com>
2024-01-18 11:01:25 +0100

    feat: log domain on each tab change

--- sw/sw-tracker.js
+++ sw/sw-tracker.js
@@ -1,1 +1,23 @@
 console.log("service worker tracker");
+
+let activeTabId, lastUrl, lastDomain;
+
+function getTabInfo(tabId) {
+  chrome.tabs.get(tabId, function (tab) {
+    if (lastUrl != tab.url || lastTitle != tab.title)
+      console.log(
+        (lastUrl = tab.url),
+        (lastDomain = new URL(tab.url).hostname)
+      );
+  });
+}
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  getTabInfo((activeTabId = activeInfo.tabId));
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (activeTabId == tabId) {
+    getTabInfo(tabId);
+  }
+});
